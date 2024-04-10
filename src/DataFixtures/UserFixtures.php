e6f224e3c84d46430b07cc227883ea910db96793
root <root@DESKTOP-FF94VLR>
2024-04-10 14:45:04 +0200

    add userEntity

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $hasher)
+    {
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail('admin@test.com');
+        $password = $this->hasher->hashPassword($user, 'pass_1234');
+        $user->setPassword($password);
+        $user->setRoles(['ROLE_USER', 'ROLE_ADMIN']);
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
