b514052b3f6debacf6fe19adca2c597036234f81
pinklady <lancelle.clara>
2024-04-15 22:57:02 +0200

    create CompanyUser && Admin && Student and fixture for each

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,7 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Administrator;
+use App\Entity\CompanyUser;
+use App\Entity\Student;
 use App\Entity\User;
+use App\Enum\UserType;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -15,13 +19,48 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        $user = new User();
+
+        //Administrator
+        $user = new Administrator();
         $user->setName('admin');
         $user->setFirstname('admin');
         $user->setEmail('admin@test.com');
         $password = $this->hasher->hashPassword($user, 'pass_1234');
         $user->setPassword($password);
+        $user->setCellphone(0164025663);
+        $user->setCity('paris');
+        $user->setZipCode(75002);
         $user->setRoles(['ROLE_USER', 'ROLE_ADMIN']);
+        $manager->persist($user);
+        $manager->flush();
+
+        //Student
+        $user = new Student();
+        $user->setName('student');
+        $user->setFirstname('test');
+        $user->setEmail('student@test.com');
+        $password = $this->hasher->hashPassword($user, 'pass_student_1234');
+        $user->setPassword($password);
+        $user->setCellphone(0164025663);
+        $user->setCity('paris');
+        $user->setZipCode(75002);
+        $user->setBirthdate(new \DateTime('2000-04-03'));
+        $user->setRoles(['ROLE_USER']);
+        $manager->persist($user);
+        $manager->flush();
+
+        //Student
+        $user = new CompanyUser();
+        $user->setName('companyUser');
+        $user->setFirstname('test');
+        $user->setEmail('companyUser@test.com');
+        $password = $this->hasher->hashPassword($user, 'pass_companyUser_1234');
+        $user->setPassword($password);
+        $user->setCellphone(0164025663);
+        $user->setCity('paris');
+        $user->setZipCode(75002);
+        $user->setPosition('CTO');
+        $user->setRoles(['ROLE_USER']);
         $manager->persist($user);
         $manager->flush();
     }
