3f2cf32192d91faf74b7b4d13a86cfd98f8cec73
pinklady <lancelle.clara>
2024-05-17 16:05:51 +0200

    add fixtures with images && update admin form

--- src/Entity/Company.php
+++ src/Entity/Company.php
@@ -87,14 +87,20 @@ class Company
     #[ORM\OneToMany(targetEntity: Offer::class, mappedBy: 'company', orphanRemoval: true)]
     private Collection $offers;
 
+    #[ORM\Column(length: 255)]
+    //#[Assert\Image]
+    private ?string $large_image = null;
+
+    #[ORM\Column(length: 255)]
+    //#[Assert\Image]
+    private ?string $picto_image = null;
+
     public function __construct()
     {
         $this->companyAdministrators = new ArrayCollection();
         $this->created_at            = new DateTimeImmutable();
         $this->updated_at            = new DateTimeImmutable();
         $this->offers                = new ArrayCollection();
-
-
     }
 
     public function getId(): ?int
@@ -391,5 +397,29 @@ class Company
     public function __toString(): string
     {
         return $this->getName();
+    }
+
+    public function getLargeImage(): ?string
+    {
+        return $this->large_image;
+    }
+
+    public function setLargeImage(string $large_image): static
+    {
+        $this->large_image = $large_image;
+
+        return $this;
+    }
+
+    public function getPictoImage(): ?string
+    {
+        return $this->picto_image;
+    }
+
+    public function setPictoImage(string $picto_image): static
+    {
+        $this->picto_image = $picto_image;
+
+        return $this;
     }
 }
