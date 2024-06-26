662c99fef052520b49619a3945e41ef9c8335e0d
pinklady <lancelle.clara>
2024-06-26 14:49:06 +0200

    replace patch by put

--- src/Entity/CompanyActivity.php
+++ src/Entity/CompanyActivity.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Put;
 use App\Repository\CompanyActivityRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -16,6 +17,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
     operations: [
         new GetCollection(),
         new Get(),
+        new Put()
     ]
 )]
 #[ORM\Entity(repositoryClass: CompanyActivityRepository::class)]
